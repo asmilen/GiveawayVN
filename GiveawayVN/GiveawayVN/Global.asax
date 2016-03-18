5c49fbeb89986ff57f0c4df015e414e76d861833
asmilen <namvu1210@gmail.com>
2016-03-18 21:19:10 +0600

    New

---
+++ GiveawayVN/GiveawayVN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GiveawayVN.MvcApplication" Language="C#" %>
---
+++ GiveawayVN/GiveawayVN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GiveawayVN.Startup))]
+namespace GiveawayVN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
